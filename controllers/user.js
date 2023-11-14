820aad1ac414e43917af91606c1381c1d5f57521
Akash <babaidutta222001@gmail.com>
2023-11-14 12:52:58 +0530

    Your user's password is at riskgit statusgit status

---
+++ controllers/user.js
@@ -0,0 +1,66 @@
+const User = require('../models/user');
+const bcrypt = require('bcrypt');
+
+function isstringvalid(string) {
+    if (string == undefined || string.length === 0) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+const signup = async (req, res) => {
+    try {
+        const { name, email, password } = req.body;
+        console.log(email, name, password);
+        if (isstringvalid(name) || isstringvalid(email) || isstringvalid(password)) {
+            return res.status(400).json({ err: "Bad parameters - Something is missing" })
+        }
+
+        const saltrounds = 10;
+        bcrypt.hash(password, saltrounds, async(err, hash) => {
+            console.log(err)
+            await User.create({name, email, password: hash})
+            res.status(201).json({message: 'Successfuly create new user'})
+        })
+    } catch (err) {
+        res.status(500).json(err);
+    }
+}
+
+const login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        if (isstringvalid(email) || isstringvalid(password)) {
+            return res.status(400).json({ message: "Email id or password is missing", success: false })
+        }
+        console.log(password);
+        const user = await User.findAll({ where: { email } })
+        if (user.length > 0) {
+            bcrypt.compare(password, user[0].password, (err, result) => {
+                if(err){
+                    res.status(500).json({ success: false, message: "Something went wrong" })
+                }
+                if(result == true){
+                res.status(200).json({ success: true, message: "User logged in sucessfully" })
+                }
+                else {
+                    return res.status(400).json({ success: false, message: "Password is incorrect" })
+                }
+            })     
+        }
+        else {
+            return res.status(404).json({ success: false, message: "User doesnot exist" })
+        }
+    }
+    catch (err) {
+        res.status(500).json({ message: err, success: false })
+    }
+}
+
+module.exports = {
+    signup,
+    login
+}
